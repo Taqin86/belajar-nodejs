7279ed69c8d78fe5fb277961841d8d5c7317377e
Taqin86 <rizqi.muttaqi@gmail.com>
2021-12-22 07:33:25 +0700

    create var, object, class n exports

--- coba.js
+++ coba.js
@@ -4,4 +4,32 @@ function cetakNama(nama) {
     return `Hallo, nama saya ${nama}`;
 }
 
-module.exports = cetakNama;
+const PI = 3.14
+
+const mahasiswa = {
+    nama : 'Rizqi Taqin', // property
+    umur : 21,
+    cetakMhs() {
+        return `Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.` ;
+    }, // method
+};
+
+class Orang {
+    constructor() {
+        console.log('Objek orang sudah di buat');
+    }
+};
+
+// module.exports.cetakNama = cetakNama;
+// module.exports.PI = PI;
+// module.exports.mahasiswa = mahasiswa;
+// module.exports.Orang = Orang; // kalau membuat class itu aturan namanya harus besar
+
+// module.exports = {
+//     cetakNama: cetakNama,
+//     PI: PI,
+//     mahasiswa: mahasiswa,
+//     Orang: Orang
+// };
+
+module.exports = {cetakNama, PI, mahasiswa, Orang};
