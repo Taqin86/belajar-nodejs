eea649be465375d2c49582c373a59a94f1f6ff0b
Taqin86 <rizqi.muttaqi@gmail.com>
2021-12-22 07:13:46 +0700

    ada yang namanya core module, local module, third party

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const cetakNama = require('./coba'); //local module
+// const fs = require('fs'); // core module
+// const cetakNama = require('./coba'); //local module
+// const moment =require('moment'); // third party / npm module / node_module
+
 
 console.log(cetakNama('Taqin'));
