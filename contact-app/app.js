ecd61f0112af44edfe9c4301084ce9177e088353
Taqin86 <rizqi.muttaqi@gmail.com>
2021-12-24 06:23:09 +0700

    first

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,36 +1,9 @@
-// Core Module
-// File System
 const fs = require('fs');
-const { stdout } = require('process');
-
-// menuliskan string ke file(synchronous)
-// try {
-//     fs.writeFileSync('data/test.txt', 'Hello World secara synchronous!');
-// } catch(e) {
-//     console.log(e)
-// }
-
-// menuslis string ke file (asynchronous)
-// fs.writeFile('data/test.txt', 'Hello world secara Asynchronous', (e) => {
-//     console.log(e);
-// });
-
-// membaca isi file(synchronous)
-// const data = fs.readFileSync('data/test.txt', 'utf-8');
-// console.log(data);
-
-// membaca isi ile(asynchronous)
-// fs.readFile('data/test.txt', 'utf-8', (e, data) => {
-//     if (e) throw e;
-//     console.log(data);
-// })
-
-
-// Readline
 const readline = require('readline');
+
 const rl = readline.createInterface({
     input: process.stdin,
-    output: stdout,
+    output: process.stdout,
 });
 
 rl.question('Masukkan nama anda : ', (nama) => {
