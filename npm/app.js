8f824a0d0bd078c232f5dd536f481f6101bd3fb9
Taqin86 <rizqi.muttaqi@gmail.com>
2021-12-24 05:47:46 +0700

    chalk

--- npm/app.js
+++ npm/app.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+const validator = require('validator');
+const chalk = require('chalk');
+
+// console.log(validator.isEmail('Taqin@gmail.com'));
+// console.log(validator.isMobilePhone('0858123456', 'id-ID'));
+// console.log(validator.isNumeric('0858123456'));
+
