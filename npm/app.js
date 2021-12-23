f04deeeafac8f52dee8bb3825ccb0782f8455d1f
Taqin86 <rizqi.muttaqi@gmail.com>
2021-12-24 05:56:47 +0700

    chalk test

--- npm/app.js
+++ npm/app.js
@@ -5,3 +5,7 @@ const chalk = require('chalk');
 // console.log(validator.isMobilePhone('0858123456', 'id-ID'));
 // console.log(validator.isNumeric('0858123456'));
 
+// console.log(chalk.italic.bgRed.white('Hello WOrld'));
+const nama = 'Taqin';
+const pesan = chalk`Hay, saya ${nama} sedang belajar {bgRed nodejs} lo, {bgRed.bold yuk belajar bareng!}`;
+console.log(pesan);
