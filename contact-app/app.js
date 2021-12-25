e3702a3c17de1068a9e810b976e14054d24efc19
Taqin86 <rizqi.muttaqi@gmail.com>
2021-12-26 06:17:09 +0700

    yargs

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,4 +1,20 @@
+// mengambil argument dari command line
+// const command = process.argv[2];
+// if(command === 'add') {
+
+// } else if (command === 'remove') {
+
+// } else if (command === 'list') {
+
+// }
+
+
+
+
+
+
 const contacts = require('./contacts');
+
 const main = async () => {
     const nama = await contacts.tulisPertanyaan('Masukkan nama anda: ');
     const email = await contacts.tulisPertanyaan('Masukkan email anda: ');
