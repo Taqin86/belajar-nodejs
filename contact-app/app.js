fac40dda3a984a576d63fd8009c47a2e859cd859
Taqin86 <rizqi.muttaqi@gmail.com>
2021-12-26 06:43:32 +0700

    menggunakan yargs

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,26 +1,29 @@
-// mengambil argument dari command line
-// const command = process.argv[2];
-// if(command === 'add') {
+const yargs = require('yargs');
+const contacts = require('./contacts')
 
-// } else if (command === 'remove') {
+yargs.command({
+    command: 'add',
+    describe: 'Menambahkan contact baru',
+    builder: {
+        nama: {
+            describe: "Nama lengkap",
+            demandOption: true,
+            type: 'string',
+        },
+        email: {
+            describe: 'Email',
+            demandOption: false,
+            type: 'string',
+        },
+        noHP: {
+            describe: 'Nomor Handphone',
+            demandOption: true,
+            type: 'string'
+        },
+    },
+    handler(argv) {
+        contacts.simpanContact(argv.nama, argv.email, argv.noHP);
+    }
+});
 
-// } else if (command === 'list') {
-
-// }
-
-
-
-
-
-
-const contacts = require('./contacts');
-
-const main = async () => {
-    const nama = await contacts.tulisPertanyaan('Masukkan nama anda: ');
-    const email = await contacts.tulisPertanyaan('Masukkan email anda: ');
-    const noHP = await contacts.tulisPertanyaan('Masukkan nomor HP anda: ');
-
-    contacts.simpanContact(nama, email, noHP);
-}
-
-main();
+yargs.parse();
