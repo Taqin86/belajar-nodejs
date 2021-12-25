5f5610ea0eb4580b0e5c663e69843007c84c9c2a
Taqin86 <rizqi.muttaqi@gmail.com>
2021-12-26 05:56:26 +0700

    first contact

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,23 +1,10 @@
-const fs = require('fs');
-const readline = require('readline');
+const contacts = require('./contacts');
+const main = async () => {
+    const nama = await contacts.tulisPertanyaan('Masukkan nama anda: ');
+    const email = await contacts.tulisPertanyaan('Masukkan email anda: ');
+    const noHP = await contacts.tulisPertanyaan('Masukkan nomor HP anda: ');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+    contacts.simpanContact(nama, email, noHP);
+}
 
-rl.question('Masukkan nama anda : ', (nama) => {
-    rl.question('masukkan nomor HP anda: ', (noHP) => {
-        const contact = { nama, noHP };
-        const file = fs.readFileSync('data/contacts.json', 'utf-8');
-        const contacts =  JSON.parse(file);
-        contacts.push(contact);
-
-        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
-
-        console.log('Terima kasih sudah mengirim data');
-
-
-        rl.close();
-    })
-})
+main();
