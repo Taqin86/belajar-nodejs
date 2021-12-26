f8805628ff33d980c1150bb572d04f870b918c8d
Taqin86 <rizqi.muttaqi@gmail.com>
2021-12-26 07:26:22 +0700

    list kontak

--- contact-app/contacts.js
+++ contact-app/contacts.js
@@ -14,11 +14,17 @@ if(!fs.existsSync(dataPath)){
     fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
+const loadContact = () => {
+    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
+    const contacts = JSON.parse(fileBuffer);
+    return contacts;
+}
 
 const simpanContact = (nama, email, noHP) => {
     const contact = {nama, email, noHP};
-    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
-    const contacts = JSON.parse(fileBuffer);
+    // const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
+    // const contacts = JSON.parse(fileBuffer);
+    const contacts = loadContact();
 
     // cek duplikat
     const duplikat = contacts.find((contact) => contact.nama === nama);
@@ -46,6 +52,14 @@ const simpanContact = (nama, email, noHP) => {
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
 
     console.log(chalk.green.inverse.bold('terima kasih sudah memaskan data.'));
+};
+
+const listContact = () => {
+    const contacts = loadContact();
+    console.log(chalk.cyan.inverse.bold('Daftar Kontak : '));
+    contacts.forEach((contact, i) => {
+        console.log(`${i + 1}. ${contact.nama} - ${contact.noHP}`)
+    })
 }
 
-module.exports = { simpanContact};
+module.exports = { simpanContact, listContact};
