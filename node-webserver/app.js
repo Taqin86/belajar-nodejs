5ce5825402421506d05cfd9698fcadeac3d30564
Taqin86 <rizqi.muttaqi@gmail.com>
2021-12-26 08:51:05 +0700

    sederhana banget

--- node-webserver/app.js
+++ node-webserver/app.js
@@ -1,8 +1,23 @@
 const http = require('http');
+const port = '3000'
 
 http.createServer((req, res)=> {
-    res.write('Hello World');
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    const url = req.url;
+    if(url === '/about') {
+        res.write('<h1>Ini adalah halaman about');
     res.end();
-}).listen(3000, () => {
-    console.log('Server is listening on port 3000..')
+    } else if(url === '/contact') {
+        res.write('<h1>Ini adalah halaman contact');
+    res.end();
+    } else {
+        res.write('Hello World');
+        res.end();
+    }
+
+}).listen(port, () => {
+    console.log(`Server is listening on port ${port}..`)
 })
