a2cb23047d77367723a33aa454efe0fa45aa64cc
Taqin86 <rizqi.muttaqi@gmail.com>
2021-12-27 06:07:07 +0700

    mencoba morgan

--- express-middleware/app.js
+++ express-middleware/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,7 @@ app.set('view engine', 'ejs');
 
 // Third-party Middleware
 app.use(expressLayouts);
+app.use(morgan('dev'));
 
 // Built-in middleware
 app.use(express.static('public'))
