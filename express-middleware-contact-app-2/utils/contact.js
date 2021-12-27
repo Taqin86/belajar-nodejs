b6c6e74ebeee113ab4432e74bca8b652ca97287e
Taqin86 <rizqi.muttaqi@gmail.com>
2021-12-27 17:49:39 +0700

    tambah data contact

--- express-middleware-contact-app-2/utils/contact.js
+++ express-middleware-contact-app-2/utils/contact.js
@@ -25,4 +25,24 @@ const findContact = (nama) => {
     const contact = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
     return contact;
 };
-module.exports = {loadContact, findContact}
+
+// menuliskan / menimpa file contacts.json dengan data yang baru
+const saveContacts = (contact) => {
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contact));
+};
+
+// menambahkan data contact baru
+const addContact = (contact) => {
+    const contacts = loadContact();
+    contacts.push(contact);
+    saveContacts(contacts);
+};
+
+
+// cek nama yang duplikat
+const cekDuplikat = (nama) => {
+    const contacts = loadContact();
+    return contacts.find((contact) => contact.nama === nama);
+}
+
+module.exports = {loadContact, findContact, addContact, cekDuplikat}
